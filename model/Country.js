47d2db479bf2f223c46370664d7f4691faa458cb
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-06-20 09:36:30 +0100

    done again

--- model/Country.js
+++ model/Country.js
@@ -14,24 +14,24 @@ const countrySchema = new Schema({
 
 const Country=mongoose.model('Country', countrySchema)
 
-async function fetchCountries(){
+// async function fetchCountries(){
 
-    const response = await fetch('https://api.crystalmol.com/api/users/list/country')
-    const countries = await response.json()
-   const {data}=countries
-   console.log(data)
-    for(let j =0; j < data.length; j++) {
-    //  for(let  i in countries) {
-        console.log(countries)
-         const country = new Country({
-           code: data[j].code,
-           name: data[j].name,
-           symbol: data[j].symbol
-           })
+//     const response = await fetch('https://api.crystalmol.com/api/users/list/country')
+//     const countries = await response.json()
+//    const {data}=countries
+//    console.log(data)
+//     for(let j =0; j < data.length; j++) {
+//     //  for(let  i in countries) {
+//         console.log(countries)
+//          const country = new Country({
+//            code: data[j].code,
+//            name: data[j].name,
+//            symbol: data[j].symbol
+//            })
 
-country.save().then(res=>console.log('saved successfully')).catch(err=>console.log(err));
-    }    
-}
+// country.save().then(res=>console.log('saved successfully')).catch(err=>console.log(err));
+//     }    
+// }
 
 
 module.exports = Country
